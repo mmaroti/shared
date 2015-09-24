ad9a153df84eddaa9cc89ab2fbd948b7e6d81701
Miklos Maroti <mmaroti@gmail.com>
2015-09-25 01:23:52 +0200

    more work on multidim arrays

--- org/mmaroti/ua/sat/GenArray.java
+++ org/mmaroti/ua/sat/GenArray.java
@@ -30,7 +30,7 @@ public class GenArray<ELEM> {
 		return shape;
 	}
 
-	public int getDim() {
+	public int getOrder() {
 		return shape.length;
 	}
 
@@ -107,6 +107,11 @@ public class GenArray<ELEM> {
 		return new GenArray<ELEM>(new int[] { size }, new int[] { 1 }, 0, es);
 	}
 
+	public static <ELEM> GenArray<ELEM> vector(final ELEM[] elems) {
+		return new GenArray<ELEM>(new int[] { elems.length }, new int[] { 1 },
+				0, elems);
+	}
+
 	public static <ELEM> GenArray<ELEM> matrix(final int[] shape,
 			final Collection<ELEM> elems) {
 		int[] steps = new int[shape.length];
@@ -130,60 +135,112 @@ public class GenArray<ELEM> {
 	}
 
 	public GenArray<ELEM> reshape(int[] shape2, int[] map) {
-		assert shape2.length == map.length;
+		assert shape.length == map.length;
 
 		int[] steps2 = new int[shape2.length];
 
 		for (int i = 0; i < map.length; i++) {
-			assert map[i] >= -1;
-			if (map[i] >= 0) {
-				assert map[i] < shape.length;
-				assert shape2[i] == shape[map[i]];
+			assert 0 <= map[i] && map[i] < shape2.length;
+			assert shape2[map[i]] == shape[i];
 
-				steps2[i] += shape[map[i]];
-			}
+			steps2[map[i]] += steps[i];
 		}
 
 		return new GenArray<ELEM>(shape2, steps2, offset, elems);
 	}
 
-	public interface Fun<ELEM, ELEM2> {
-		public ELEM2 map(ELEM elem);
+	public interface Fun<ELEM, ELEM1> {
+		public ELEM call(ELEM1 elem);
 	}
 
-	public <ELEM2> GenArray<ELEM2> map(Fun<ELEM, ELEM2> fun) {
-		int[] steps2 = new int[shape.length];
+	public static <ELEM, ELEM1> GenArray<ELEM> map(Fun<ELEM, ELEM1> fun,
+			GenArray<ELEM1> arg) {
+		int[] shape = arg.shape;
+		int[] steps = new int[shape.length];
 
 		int size = 1;
 		for (int i = 0; i < shape.length; i++) {
 			assert 0 <= shape[i];
-			steps2[i] = size;
-			size *= shape[i];
+
+			if (arg.steps[i] != 0) {
+				steps[i] = size;
+				size *= shape[i];
+			}
 		}
 
 		@SuppressWarnings("unchecked")
-		ELEM2[] elems2 = (ELEM2[]) new Object[size];
+		ELEM[] elems = (ELEM[]) new Object[size];
 
-		int[] index = new int[shape.length];
-		int pos1 = offset;
-		int pos2 = 0;
-		while (pos2 < size) {
-			elems2[pos2] = fun.map(elems[pos1]);
-			pos2 += 1;
+		int[] index = new int[steps.length];
+		int pos = 0;
+		while (pos < size) {
+			elems[pos] = fun.call(arg.get(index));
 
+			pos += 1;
 			for (int i = 0; i < index.length; i++) {
-				pos1 += steps[i];
-				index[i] += 1;
-
-				if (index[i] == shape[i]) {
-					pos1 -= shape[i] * steps[i];
+				if (steps[i] == 0)
+					;
+				else if (++index[i] >= shape[i])
 					index[i] = 0;
-				} else
+				else
 					break;
 			}
 		}
 
-		return new GenArray<ELEM2>(shape, steps2, 0, elems2);
+		return new GenArray<ELEM>(shape, steps, 0, elems);
+	}
+
+	public interface Fun2<ELEM, ELEM1, ELEM2> {
+		public ELEM call(ELEM1 elem1, ELEM2 elem2);
+	}
+
+	public static <ELEM, ELEM1, ELEM2> GenArray<ELEM> map(
+			Fun2<ELEM, ELEM1, ELEM2> fun, GenArray<ELEM1> arg1,
+			GenArray<ELEM2> arg2) {
+		assert arg1.shape.length == arg2.shape.length;
+
+		int[] shape = arg1.shape;
+		int[] steps = new int[shape.length];
+
+		int size = 1;
+		for (int i = 0; i < shape.length; i++) {
+			assert 0 <= shape[i] && shape[i] == arg2.shape[i];
+
+			if (arg1.steps[i] != 0 || arg2.steps[i] != 0) {
+				steps[i] = size;
+				size *= shape[i];
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		ELEM[] elems = (ELEM[]) new Object[size];
+
+		int[] index = new int[steps.length];
+		int pos = 0;
+		while (pos < size) {
+			elems[pos] = fun.call(arg1.get(index), arg2.get(index));
+
+			pos += 1;
+			for (int i = 0; i < index.length; i++) {
+				if (steps[i] == 0)
+					;
+				else if (++index[i] >= shape[i])
+					index[i] = 0;
+				else
+					break;
+			}
+		}
+
+		return new GenArray<ELEM>(shape, steps, 0, elems);
+	}
+
+	public interface Monoid<ELEM> extends Fun2<ELEM, ELEM, ELEM> {
+		public ELEM unit();
+	}
+
+	public static <ELEM> GenArray<ELEM> map(Monoid<ELEM> monoid,
+			GenArray<ELEM>[] args) {
+		return null;
 	}
 
 	public String toString() {
@@ -212,6 +269,115 @@ public class GenArray<ELEM> {
 		return str.toString();
 	}
 
+	public final static Fun<Integer, Integer> INT_ID = new Fun<Integer, Integer>() {
+		@Override
+		public Integer call(Integer elem) {
+			return elem;
+		}
+	};
+
+	public final static Fun<Integer, Integer> INT_NEG = new Fun<Integer, Integer>() {
+		@Override
+		public Integer call(Integer elem) {
+			return -elem;
+		}
+	};
+
+	public final static Fun<Boolean, Boolean> BOOL_ID = new Fun<Boolean, Boolean>() {
+		@Override
+		public Boolean call(Boolean elem) {
+			return elem;
+		}
+	};
+
+	public final static Fun<Boolean, Boolean> BOOL_NEG = new Fun<Boolean, Boolean>() {
+		@Override
+		public Boolean call(Boolean elem) {
+			return !elem;
+		}
+	};
+
+	public final static Monoid<Integer> INT_ADD = new Monoid<Integer>() {
+		@Override
+		public Integer unit() {
+			return 0;
+		}
+
+		@Override
+		public Integer call(Integer elem1, Integer elem2) {
+			return elem1 + elem2;
+		}
+	};
+
+	public final static Monoid<Integer> INT_MUL = new Monoid<Integer>() {
+		@Override
+		public Integer unit() {
+			return 1;
+		}
+
+		@Override
+		public Integer call(Integer elem1, Integer elem2) {
+			return elem1 * elem2;
+		}
+	};
+
+	public final static Fun2<Boolean, Integer, Integer> INT_EQ = new Fun2<Boolean, Integer, Integer>() {
+		@Override
+		public Boolean call(Integer elem1, Integer elem2) {
+			return elem1.intValue() == elem2.intValue();
+		}
+	};
+
+	public final static Fun2<Boolean, Integer, Integer> INT_LEQ = new Fun2<Boolean, Integer, Integer>() {
+		@Override
+		public Boolean call(Integer elem1, Integer elem2) {
+			return elem1.intValue() <= elem2.intValue();
+		}
+	};
+
+	public final static Monoid<Boolean> BOOL_AND = new Monoid<Boolean>() {
+		@Override
+		public Boolean unit() {
+			return Boolean.TRUE;
+		}
+
+		@Override
+		public Boolean call(Boolean elem1, Boolean elem2) {
+			return elem1 && elem2;
+		}
+	};
+
+	public final static Monoid<Boolean> BOOL_OR = new Monoid<Boolean>() {
+		@Override
+		public Boolean unit() {
+			return Boolean.FALSE;
+		}
+
+		@Override
+		public Boolean call(Boolean elem1, Boolean elem2) {
+			return elem1 || elem2;
+		}
+	};
+
+	public final static Monoid<Boolean> BOOL_ADD = new Monoid<Boolean>() {
+		@Override
+		public Boolean unit() {
+			return Boolean.FALSE;
+		}
+
+		@Override
+		public Boolean call(Boolean elem1, Boolean elem2) {
+			return elem1 ? !elem2 : elem2;
+		}
+	};
+
+	public final static Fun2<Boolean, Boolean, Boolean> BOOL_IMP = new Fun2<Boolean, Boolean, Boolean>() {
+		@Override
+		public Boolean call(Boolean elem1, Boolean elem2) {
+			return !elem1 || elem2;
+		}
+	};
+
 	public static void main(String[] args) {
 		GenArray<Integer> array = GenArray.generate(new int[] { 3, 3 },
 				new Gen<Integer>() {
@@ -222,7 +388,19 @@ public class GenArray<ELEM> {
 				});
 		System.out.println(array);
 
-		array = GenArray.constant(new int[] { 2, 3 }, 0);
+		array = array.reshape(new int[] { 2, 3, 2 }, new int[] { 1, 1 });
+		System.out.println(array);
+
+		// array = GenArray.map(INT_ID, array);
+		// System.out.println(array);
+
+		// array = array.reshape(new int[] { 2, 3 }, new int[] { 0, 1, 0 });
+		// System.out.println(array);
+
+		// array = GenArray.vector(new Integer[] { 1, 2, 3 });
+		// System.out.println(array);
+
+		array = GenArray.map(INT_ADD, array, array);
 		System.out.println(array);
 	}
 }
